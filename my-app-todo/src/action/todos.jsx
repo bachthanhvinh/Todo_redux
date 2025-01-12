55bed4b54236d68cf281c50389e4e33419b653e7
Vinhz <bachthanhvinh12@gmail.com>
2025-01-12 22:25:49 +0700

    thêm đã hoàn thành và hủy hoàn thành

--- my-app-todo/src/action/todos.jsx
+++ my-app-todo/src/action/todos.jsx
@@ -1,5 +1,20 @@
-export const createTodo = (value) => ({
-  type: "CREATETODO",
-  content: value,
-  completed: false,
-});
+export const createTodo = (value) => {
+  return {
+    type: "CREATETODO",
+    content: value,
+  };
+};
+
+export const todoHandle = (id) => {
+  return {
+    id: id,
+    type: "TODOHANDLE",
+  };
+};
+
+export const undoHandle = (id) => {
+  return {
+    id: id,
+    type: "UNDOHANDLE",
+  };
+};
--- my-app-todo/src/reducers/todos.jsx
+++ my-app-todo/src/reducers/todos.jsx
@@ -2,7 +2,7 @@ const init = [
   {
     id: 1,
     content: "Công việc 1",
-    completed: false,
+    completed: true,
   },
   {
     id: 2,
@@ -25,9 +25,22 @@ const todoReduce = (state = init, action) => {
         {
           id: Date.now(),
           content: action.content,
+          completed: false,
         },
       ];
+      return newState;
 
+    case "TODOHANDLE":
+      const index = newState.findIndex((item) => {
+        return item.id === action.id;
+      });
+      newState[index].completed = true;
+      return newState;
+    case "UNDOHANDLE":
+      const undoindex = newState.findIndex((item) => {
+        return item.id === action.id;
+      });
+      newState[undoindex].completed = false;
       return newState;
     default:
       return state;
